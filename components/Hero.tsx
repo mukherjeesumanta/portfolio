d4f7966f7efd48645e7188ad06a30af579b4fcd3
Sumanta Mukherjee <sumanta.mukherjee.kat@gmail.com>
2023-02-16 00:53:04 +0530

    myportfolio first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles';
+
+type Props = {}
+
+const Hero = (props: Props) => {
+
+  const [text, count] = useTypewriter({
+    words: [
+      'Hi, the name\'s Sumanta',
+      'The guy who loves Coffee',
+      '<ButLovesToCodeMore />'
+    ],
+    loop: true,
+    delaySpeed: 2000
+  });
+
+  return (
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor='#f7ab0a' />
+      </h1>
+    </div>
+  )
+}
+
+export default Hero;
